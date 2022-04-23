5c1898d3a47405753f0087ef1cfc18159830e8a6
dslee <leeds019@naver.com>
2022-04-23 15:47:18 +0900

    nunjuck 셋팅 변경, 파일명 변경

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 node -v
 v14.18.1
 
+npm install gulp -g
+
 npm i
 
 gulp
--- gulpfile.js
+++ gulpfile.js
@@ -31,46 +31,21 @@ const apfBrwsowsers = [
 
 // CLEAN
 gulp.task('clean',() => {
-    return del('./dist');
+    return del('dist');
 })
 
 // FONT
 gulp.task('fonts',() => {
-    return gulp.src('./src/assets/fonts/*.*')
-        .pipe(gulp.dest('./dist/assets/fonts'))
+    return gulp.src('src/assets/fonts/*.*')
+        .pipe(gulp.dest('dist/assets/fonts'))
         .pipe(browserSync.reload({ stream : true }));
 });
 
 // IMAGES
 gulp.task('images',() => {
-    return gulp.src('./src/assets/images/**/*.*')
-        .pipe(cache(imagemin([
-            //png
-            imageminPngquant({
-                speed: 1,
-                quality: [0.95, 1] //lossy settings
-            }),
-            imageminZopfli({
-                more: true
-                // iterations: 50 // very slow but more effective
-            }),
-            imageminGiflossy({
-                optimizationLevel: 3,
-                optimize: 3, //keep-empty: Preserve empty transparent frames
-                lossy: 2
-            }),
-            //svg
-            imagemin.svgo({
-                plugins: [{
-                    removeViewBox: false
-                }]
-            }),
-            //jpg very light lossy, use vs jpegtran
-            imageminMozjpeg({
-                quality: 90
-            })
-        ])))
-        .pipe(gulp.dest('./dist/assets/images'))
+    return gulp.src(['src/assets/images/**/*.*'])
+        .pipe(cache(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/assets/images'))
         .pipe(browserSync.reload({ stream : true }));
 });
 
@@ -85,12 +60,12 @@ gulp.task( 'scss', () => {
         },
         postcss: [ autoprefixer ({overrideBrowserslist: apfBrwsowsers,})]
     };
-    return gulp.src('./src/assets/scss/*.scss')
+    return gulp.src('src/assets/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(scss({fiber:Fiber}).on('error', scss.logError))
         .pipe(postcss(options.postcss))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/assets/css'))
+        .pipe(gulp.dest('dist/assets/css'))
         .pipe(browserSync.reload({ stream : true }));
 })
 
@@ -100,50 +75,50 @@ gulp.task('html', () => {
         indentSize: 2
     };
     return gulp.src([
-        './src/page/*',
-        '!' + './src/page/include' ])
-        .pipe(fileInclude({prefix: '@@', basepath: './src/page/include'}))
+        'src/page/*',
+        '!' + 'src/page/include' ])
+        .pipe(fileInclude({prefix: '@@', basepath: 'src/page/include'}))
         .pipe(data(function() {
             return require('./src/data.json')
         }))
         .pipe(nunjucksRender({
-            path: ['./src/templates']
+            path: ['src/templates']
         }))
         .pipe(htmlbeautify(options))
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('dist'))
         .pipe(browserSync.reload({ stream : true }));
 })
 
 // JS
 gulp.task('script', () => {
-    return gulp.src('./src/assets/js/**/*.js')
+    return gulp.src('src/assets/js/**/*.js')
         .pipe(babel({
             'presets':[
                 '@babel/preset-env'
             ]
         }))
-        .pipe(gulp.dest('./dist/assets/js'))
+        .pipe(gulp.dest('dist/assets/js'))
         .pipe(browserSync.reload({ stream : true }));
 });
 
 // VENDOR JS
 gulp.task('vendor:js', () => {
-    return gulp.src('./src/assets/vendor/js/*.js')
+    return gulp.src('src/assets/vendor/js/*.js')
         .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('./dist/assets/js/vendor'))
+        .pipe(gulp.dest('dist/assets/js/vendor'))
         .pipe(browserSync.reload({ stream : true }));
 });
 
 // VENDOR JS MAP
 gulp.task('vendor:js map', () => {
-    return gulp.src('./src/assets/vendor/js/*.map')
-        .pipe(gulp.dest('./dist/assets/js/vendor'))
+    return gulp.src('src/assets/vendor/js/*.map')
+        .pipe(gulp.dest('dist/assets/js/vendor'))
         .pipe(browserSync.reload({ stream : true }));
 });
 
 // VENDOR CSS
 gulp.task('vendor:css', () => {
-    return gulp.src('./src/assets/vendor/css/*.css')
+    return gulp.src('src/assets/vendor/css/*.css')
         .pipe(sourcemaps.init())
         .pipe(concatCss('vendor.css'))
         .pipe(cleanCSS({debug: true}, (details) => {
@@ -151,7 +126,7 @@ gulp.task('vendor:css', () => {
             console.log(`${details.name}: ${`minify size : ` + details.stats.minifiedSize}`);
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/assets/css/vendor'))
+        .pipe(gulp.dest('dist/assets/css/vendor'))
         .pipe(browserSync.reload({ stream : true }));
 });
 
@@ -175,14 +150,14 @@ gulp.task('browserSync', () => {
 // WATCH
 gulp.task('watch', () =>  {
     return [
-        gulp.watch('./src/assets/images/**/*.*', gulp.series('images')),
-        gulp.watch('./src/assets/fonts/**/*.*', gulp.series('fonts')),
-        gulp.watch('./src/assets/scss/**/**/*.scss', gulp.series('scss')),
-        gulp.watch('./src/assets/vendor/js/**/*.js', gulp.series('vendor:js')),
-        gulp.watch('./src/assets/vendor/css/**/*.js', gulp.series('vendor:css')),
-        gulp.watch('./src/page/**/*.html', gulp.series('html')),
-        gulp.watch('./src/templates/**/*.*', gulp.series('html')),
-        gulp.watch('./src/assets/js/**/*.js', gulp.series('script'))
+        gulp.watch('src/assets/images/**/*.*', gulp.series('images')),
+        gulp.watch('src/assets/fonts/**/*.*', gulp.series('fonts')),
+        gulp.watch('src/assets/scss/**/**/*.scss', gulp.series('scss')),
+        gulp.watch('src/assets/vendor/js/**/*.js', gulp.series('vendor:js')),
+        gulp.watch('src/assets/vendor/css/**/*.js', gulp.series('vendor:css')),
+        gulp.watch('src/page/**/*.html', gulp.series('html')),
+        gulp.watch('src/templates/**/*.*', gulp.series('html')),
+        gulp.watch('src/assets/js/**/*.js', gulp.series('script'))
     ]
 
 });
@@ -196,7 +171,6 @@ const series = gulp.series([
     'images',
     'fonts',
     'scss',
-   /* 'nunjucks',*/
     'html',
     'script',
     gulp.parallel('webServer','browserSync','watch')
