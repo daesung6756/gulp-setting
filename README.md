d5d0e7b82a29b0d3ae608148b8815c167089478d
dslee <leeds019@naver.com>
2022-04-24 23:46:01 +0900

    sprite 이미지처리 기능 추가

--- README.md
+++ README.md
@@ -6,3 +6,55 @@ npm install gulp -g
 npm i
 
 gulp
+
+------------------------------
+1. 폴더 구조
+/dist
+├─assets
+│  ├─fonts
+│  ├─images
+│  │  ├─favicons
+│  │  └─icons
+│  ├─js
+│  │  └─vendor
+│  └─css
+│     └─vendor
+└─guide
+
+/src
+├─assets
+│  ├─fonts
+│  ├─images
+│  │  ├─favicons
+│  │  └─icons
+│  ├─js
+│  ├─scss
+│  │  ├─components
+│  │  └─import
+│  └─vendor
+│      ├─css
+│      └─js
+├─page
+│  └─guide
+└─templates
+      ├─include
+      └─macros 
+
+
+2. 파일 설명
+- .gitignore 깃 이그놀 파일에는 깃 에서 관리 안하겠다고 선언한 파일들이 들어있습니다.
+깃 프로젝트가 아니면 삭제하셔도 됩니다.
+- gulpfile.js 이것은 걸프사용 js이며, 모든 테스크 규칙을 정의하는 파일입니다.
+- package.json 은 npm init 명령어로 생성이 되며, npm 설치 내역과 세부 내용
+들이 들어 있습니다.
+- server.js 는 express을 통해 가상 웹서버를 구현하는 용도 입니다.
+
+3. Module nunjuck 과 data.json
+예) {% set items = ["a", 1, { b : true}] %}
+각 페이지에 상단에 json 형태의 데이터를 사용하여 처리 하거나.
+data.json으로 처리 하셔도 됩니다.
+위의 두 방식 중 편하신걸로 사용하시면 됩니다.
+
+nunjuck 의 명령어와 사용법은 아래 경로에서 확인.
+https://mozilla.github.io/nunjucks/templating.html
+https://velog.io/@pkbird/Nunjucks-basic
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,32 @@
 "use strict";
 // 요청 모듈 정의 ES
-const gulp = require('gulp'),
-      cache = require('gulp-cache'),
-      scss = require('gulp-sass')(require('sass')),
-      Fiber = require('fibers'),
-      dartSsss = require('dart-sass'),
-      sourcemaps = require ('gulp-sourcemaps'),
-      postcss = require ('gulp-postcss'),
-      autoprefixer = require ('autoprefixer'),
-      nodemon = require('gulp-nodemon'),
-      browserSync = require('browser-sync').create(),
-      babel = require('gulp-babel'),
-      del = require('del'),
-      concat = require('gulp-concat'),
-      concatCss = require('gulp-concat-css'),
-      cleanCSS = require('gulp-clean-css'),
-      nunjucksRender = require('gulp-nunjucks-render'),
-      data = require('gulp-data'),
-      imagemin = require('gulp-imagemin'),
-      htmlbeautify = require('gulp-html-beautify');
+    const
+        gulp = require('gulp'),
+        cache = require('gulp-cache'),
+        scss = require('gulp-sass')(require('sass')),
+        Fiber = require('fibers'),
+        dartSsss = require('dart-sass'),
+        sourcemaps = require ('gulp-sourcemaps'),
+        postcss = require ('gulp-postcss'),
+        autoprefixer = require ('autoprefixer'),
+        nodemon = require('gulp-nodemon'),
+        browserSync = require('browser-sync').create(),
+        babel = require('gulp-babel'),
+        del = require('del'),
+        concat = require('gulp-concat'),
+        concatCss = require('gulp-concat-css'),
+        cleanCSS = require('gulp-clean-css'),
+        nunjucksRender = require('gulp-nunjucks-render'),
+        data = require('gulp-data'),
+        imagemin = require('gulp-imagemin'),
+        htmlbeautify = require('gulp-html-beautify'),
+        plumber = require('gulp-plumber'),
+        spritesmith = require('gulp.spritesmith'),
+        buffer = require('vinyl-buffer'),
+        merge = require('merge-stream');
 
 const apfBrwsowsers = [
-    'ie >= 8',
+    'ie >= 11',
 ];
 
 // CLEAN
@@ -32,6 +37,7 @@ gulp.task('clean',() => {
 // FONT
 gulp.task('fonts',() => {
     return gulp.src('src/assets/fonts/*.*')
+        .pipe(plumber())
         .pipe(gulp.dest('dist/assets/fonts'))
         .pipe(browserSync.reload({ stream : true }));
 });
@@ -39,25 +45,64 @@ gulp.task('fonts',() => {
 // IMAGES
 gulp.task('images',() => {
     return gulp.src(['src/assets/images/**/*.*'])
+        .pipe(plumber())
         .pipe(cache(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true })))
         .pipe(gulp.dest('dist/assets/images'))
         .pipe(browserSync.reload({ stream : true }));
 });
 
+// SPRITE IMAGES 명령어 gulp sprite
+gulp.task('sprite', function() {
+    const spConfig = {
+        targetImgPath: './src/assets/sprite/*.png',
+        targetRetinaImgPath: './src/assets/sprite/*@2x.png',
+        destImgName: 'sprite.png',
+        destRetinaImgName: 'sprite2x.png',
+        destCssName: '_sprite.scss',
+        destImgPath: './dist/assets/images/sprite',
+        destCssPath: './src/assets/scss/import',
+        destCssTemplate:'./src/assets/sprite/handlebarsStr.css.handlebars'
+    }
+    const spriteData = gulp.src(spConfig.targetImgPath)
+        .pipe(plumber())
+        .pipe(spritesmith({
+            retinaSrcFilter: spConfig.targetRetinaImgPath,
+            imgName: spConfig.destImgName,
+            retinaImgName: spConfig.destRetinaImgName,
+            padding: 5,
+            cssName: spConfig.destCssName,
+            cssTemplate: spConfig.destCssTemplate,
+        }));
+
+    const imgStream = spriteData.img
+        .pipe(buffer())
+        .pipe(plumber())
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest(spConfig.destImgPath));
+
+    const cssStream = spriteData.css
+        .pipe(buffer())
+        .pipe(plumber())
+        .pipe(gulp.dest(spConfig.destCssPath));
+
+    return merge(imgStream, cssStream)
+});
+
 // SCSS
-gulp.task( 'scss', () => {
+gulp.task( 'scss:compiler', () => {
     const options = {
-        scss : {
-            outputStyle: 'expanded',
-            indentType: 'space',
-            indentWidth: 1,
-            compiler: dartSsss,
-        },
         postcss: [ autoprefixer ({overrideBrowserslist: apfBrwsowsers,})]
     };
     return gulp.src('src/assets/scss/*.scss')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(scss({fiber:Fiber}).on('error', scss.logError))
+        .pipe(scss({
+                fiber:Fiber,
+                outputStyle: 'expanded', //compressed , expanded ,compact ,nested
+                indentType: 'space',
+                indentWidth: 1,
+                compiler: dartSsss,
+            }).on('error', scss.logError))
         .pipe(postcss(options.postcss))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/assets/css'))
@@ -69,9 +114,8 @@ gulp.task('html', () => {
     const options = {
         indentSize: 2
     };
-    return gulp.src([
-        'src/page/*',
-        '!' + 'src/page/include' ])
+    return gulp.src('src/page/*')
+        .pipe(plumber())
         .pipe(data(function() {
             return require('./src/data.json')
         }))
@@ -84,8 +128,9 @@ gulp.task('html', () => {
 })
 
 // JS
-gulp.task('script', () => {
+gulp.task('script:compiler', () => {
     return gulp.src('src/assets/js/**/*.js')
+        .pipe(plumber())
         .pipe(babel({
             'presets':[
                 '@babel/preset-env'
@@ -98,6 +143,7 @@ gulp.task('script', () => {
 // VENDOR JS
 gulp.task('vendor:js', () => {
     return gulp.src('src/assets/vendor/js/*.js')
+        .pipe(plumber())
         .pipe(concat('vendor.js'))
         .pipe(gulp.dest('dist/assets/js/vendor'))
         .pipe(browserSync.reload({ stream : true }));
@@ -106,6 +152,7 @@ gulp.task('vendor:js', () => {
 // VENDOR JS MAP
 gulp.task('vendor:js map', () => {
     return gulp.src('src/assets/vendor/js/*.map')
+        .pipe(plumber())
         .pipe(gulp.dest('dist/assets/js/vendor'))
         .pipe(browserSync.reload({ stream : true }));
 });
@@ -113,6 +160,7 @@ gulp.task('vendor:js map', () => {
 // VENDOR CSS
 gulp.task('vendor:css', () => {
     return gulp.src('src/assets/vendor/css/*.css')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(concatCss('vendor.css'))
         .pipe(cleanCSS({debug: true}, (details) => {
@@ -146,12 +194,14 @@ gulp.task('watch', () =>  {
     return [
         gulp.watch('src/assets/images/**/*.*', gulp.series('images')),
         gulp.watch('src/assets/fonts/**/*.*', gulp.series('fonts')),
-        gulp.watch('src/assets/scss/**/**/*.scss', gulp.series('scss')),
+        gulp.watch('src/assets/scss/**/**/*.scss', gulp.series('scss:compiler')),
         gulp.watch('src/assets/vendor/js/**/*.js', gulp.series('vendor:js')),
         gulp.watch('src/assets/vendor/css/**/*.js', gulp.series('vendor:css')),
         gulp.watch('src/page/**/*.html', gulp.series('html')),
         gulp.watch('src/templates/**/*.*', gulp.series('html')),
-        gulp.watch('src/assets/js/**/*.js', gulp.series('script'))
+        gulp.watch('src/**/*.json', gulp.series('html')),
+        gulp.watch('src/assets/js/**/*.js', gulp.series('script:compiler')),
+        gulp.watch('src/assets/sprite/**/*.png', gulp.series('sprite'))
     ]
 
 });
@@ -163,10 +213,11 @@ const series = gulp.series([
     'vendor:js map',
     'vendor:css',
     'images',
+    'sprite',
     'fonts',
-    'scss',
+    'scss:compiler',
     'html',
-    'script',
+    'script:compiler',
     gulp.parallel('webServer','browserSync','watch')
 ])
 
